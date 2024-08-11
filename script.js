ce3465a950ba3fc9c6d4b320674b141840a07afa
dharshinisakthivel510 <129201414+dharshinisakthivel510@users.noreply.github.com>
2024-08-11 14:23:19 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("this is script file")
+let inputField = document.getElementById("userInput")
+let button = document.getElementById("addButton")
+let noteslist = document.getElementById("notesList")
+
+function addNotes(){
+    let inputValue=inputField.value.trim()
+    if (inputValue!==""){
+    let list = document.createElement("li")
+    list.textContent= inputField.value.trim()
+    noteslist.appendChild(list);
+    console.log(inputField.value)}
+}
